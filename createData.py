fb78e0fa66f5528dfac35194ddef58302e679012
Srikanth Patala <patala@srikanths-mbp-2.ads.northwestern.edu>
2026-07-01 09:32:01 -0500

    Added csv data

---
+++ createData.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+def create_data(num_samples=1000, start_time = "2026-06-01 12:00:00"):
+    
+    # 1. Generate timestamps incremented by 10 seconds
+    time_stamps = pd.date_range(start=start_time, periods=num_samples, freq="10s")
+
+    # 2. Generate i.i.d. N(0, 1) standard normal distribution data
+    data = np.random.normal(loc=0.0, scale=1.0, size=num_samples)
+
+    # 3. Create the DataFrame
+    df = pd.DataFrame({
+        "time_stamp": time_stamps,
+        "data": data
+    })
+
+    return df
+
+csvF = "generated_data.csv"
+
+df = create_data(100000)
+df.to_csv(csvF, index_label="index")
+
+print("CSV file 'generated_data.csv' created successfully!")
+
